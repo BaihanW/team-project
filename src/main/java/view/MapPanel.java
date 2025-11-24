bff7577d9141b5fc01432a668efb440e7448eeca
unknown <white040707@gmail.com>
2025-11-24 15:49:34 -0500

    Added the functionality to the Mappanel view

--- src/main/java/view/MapPanel.java
+++ src/main/java/view/MapPanel.java
@@ -4,17 +4,30 @@ import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.OSMTileFactoryInfo;
 import org.jxmapviewer.input.PanMouseInputListener;
 import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.Waypoint;
+import org.jxmapviewer.viewer.WaypointPainter;
+
 import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * contain map tile
+
  */
 public class MapPanel extends JPanel {
 
     private final JXMapViewer mapViewer;
+
+    private final Set<Waypoint> waypoints = new HashSet<>();
+
+    private final WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<>();
 
     public MapPanel() {
         setLayout(new BorderLayout());
@@ -29,10 +42,16 @@ public class MapPanel extends JPanel {
         mapViewer.setAddressLocation(new GeoPosition(43.6532, -79.3832));
         mapViewer.setZoom(5);
 
+
+        waypointPainter.setWaypoints(waypoints);
+        mapViewer.setOverlayPainter(waypointPainter);
+
         add(mapViewer, BorderLayout.CENTER);
 
-        // mouse drag
         enableDragging();
+
+
+        enableClickToAddMarker();
     }
 
     private void enableDragging() {
@@ -41,7 +60,18 @@ public class MapPanel extends JPanel {
         mapViewer.addMouseMotionListener(mia);
     }
 
-    /** set center */
+
+    private void enableClickToAddMarker() {
+        mapViewer.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                GeoPosition gp = mapViewer.convertPointToGeoPosition(e.getPoint());
+                addMarker(gp);
+            }
+        });
+    }
+
+
     public void setCenter(double latitude, double longitude) {
         mapViewer.setAddressLocation(new GeoPosition(latitude, longitude));
         mapViewer.repaint();
@@ -50,6 +80,14 @@ public class MapPanel extends JPanel {
     public JXMapViewer getMapViewer() {
         return mapViewer;
     }
+
+
+    private void addMarker(GeoPosition position) {
+        waypoints.add(new DefaultWaypoint(position));
+        waypointPainter.setWaypoints(waypoints);
+        mapViewer.repaint();
+    }
+
     public static class HttpsOsmTileFactoryInfo extends OSMTileFactoryInfo {
         public HttpsOsmTileFactoryInfo() {
             super("OpenStreetMap HTTPS",
@@ -58,5 +96,7 @@ public class MapPanel extends JPanel {
         }
     }
 }
+
+
 
 
